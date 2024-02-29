5a9fd58b55fa2a7785a07da565149c872e23f691
Barnabás Ágoston <agostonbarna@users.noreply.github.com>
2024-02-29 18:13:05 +0100

    Simplify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 find public_prod -mindepth 1 -maxdepth 1 -not -path '*/.git*' -exec rm -rf {} +
 hugo --gc --minify -e production -d public_prod
 
-git -C public_prod add .
+cd public_prod
+git add .
 
 if [[ "${1:-}" == amend ]]; then
-  git -C public_prod commit --amend -C HEAD
-  git -C public_prod push --force-with-lease
+  git commit --amend -C HEAD
+  git push --force-with-lease
 else
-  git -C public_prod commit
-  git -C public_prod push
+  git commit
+  git push
 fi
